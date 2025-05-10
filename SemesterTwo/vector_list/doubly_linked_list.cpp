2f503af2242c108e4e7bf13915b2f6fdf62bf094
K3nch <gecayon.k3nch@yandex.ru>
2025-05-10 17:37:09 +0300

    added first ver. of vector and list classes

---
+++ SemesterTwo/vector_list/doubly_linked_list.cpp
@@ -0,0 +1,100 @@
+#include "doubly_linked_list.hpp"
+
+#include <iostream>
+
+using biv::DoublyLinkedList;
+
+template<typename T>
+DoublyLinkedList<T>::~DoublyLinkedList() {
+	Node* current = begin;
+	while (current != nullptr) {
+		Node* next = current->next;
+		delete current;
+		current = next;
+	}
+	begin = nullptr;
+	end = nullptr;
+}
+
+template<typename T>
+std::size_t DoublyLinkedList<T>::get_size() const noexcept {
+	std::size_t size = 0;
+	Node* current = begin;
+	while (current != nullptr) {
+		size++;
+		current = current->next;
+	}
+	return size;
+}
+
+template<typename T>
+bool DoublyLinkedList<T>::has_item(const T& value) const noexcept {
+	Node* current = begin;
+	while (current != nullptr) {
+		if (current->value == value) {
+			return true;
+		}
+		current = current->next;
+	}
+	return false;
+}
+
+template<typename T>
+void DoublyLinkedList<T>::print() const noexcept {
+	Node* current = begin;
+	while (current != nullptr) {
+		std::cout << current->value << " ";
+		current = current->next;
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+void DoublyLinkedList<T>::push_back(const T& value) {
+	Node* new_node = new Node(value);
+	if (begin == nullptr) {
+		begin = new_node;
+		end = new_node;
+	} else {
+		end->next = new_node;
+		new_node->prev = end;
+		end = new_node;
+	}
+}
+
+template<typename T>
+bool DoublyLinkedList<T>::remove_first(const T& value) noexcept {
+	Node* current = begin;
+	while (current != nullptr) {
+		if (current->value == value) {
+			if (current->prev != nullptr) {
+				current->prev->next = current->next;
+			} else {
+				begin = current->next;
+			}
+			if (current->next != nullptr) {
+				current->next->prev = current->prev;
+			} else {
+				end = current->prev;
+			}
+			delete current;
+			return true;
+		}
+		current = current->next;
+	}
+	std::cout << "Value not found: " << value << std::endl;
+	return false;
+}
+
+
+int main() {
+	DoublyLinkedList<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.print();
+	std::cout << "Size: " << list.get_size() << std::endl;
+	list.remove_first(2);
+	if (!list.has_item(2)) list.print();
+	std::cout << "Size: " << list.get_size() << std::endl;
+}
---
+++ SemesterTwo/vector_list/vector.cpp
@@ -0,0 +1,116 @@
+#include "vector.hpp"
+
+#include <iostream>
+
+using biv::Vector;
+
+template<typename T>
+const std::size_t Vector<T>::START_CAPACITY = 10;
+
+template<typename T>
+Vector<T>::Vector() {
+	arr = new T[START_CAPACITY];
+}
+
+template<typename T>
+Vector<T>::~Vector() {
+	if (arr != nullptr) {
+		delete[] arr;
+		arr = nullptr;
+	}
+	capacity = 0;
+	size = 0;
+}
+
+template<typename T>
+std::size_t Vector<T>::get_size() const noexcept {
+	return size;
+}
+
+template<typename T>
+bool Vector<T>::has_item(const T& value) const noexcept {
+	for (std::size_t i = 0; i < size; ++i) {
+		if (arr[i] == value) {
+			return true;
+		}
+	}
+	return false;
+}
+
+template<typename T>
+bool Vector<T>::insert(const std::size_t position, const T& value) {
+	if (position > size) {
+		return false;
+	}
+
+	if (size == capacity) {
+		capacity *= 2;
+		T* new_arr = new T[capacity];
+		for (std::size_t i = 0; i < size; ++i) {
+			new_arr[i] = arr[i];
+		}
+		delete[] arr;
+		arr = new_arr;
+	}
+
+	for (std::size_t i = size; i > position; --i) {
+		arr[i] = arr[i - 1];
+	}
+	arr[position] = value;
+	size++;
+	return true;
+}
+
+template<typename T>
+void Vector<T>::print() const noexcept {
+	for (std::size_t i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+void Vector<T>::push_back(const T& value) {
+	if (size == capacity) {
+		capacity *= 2;
+		T* new_arr = new T[capacity];
+		for (std::size_t i = 0; i < size; ++i) {
+			new_arr[i] = arr[i];
+		}
+		delete[] arr;
+		arr = new_arr;
+	}
+	arr[size] = value;
+	size++;
+}
+
+template<typename T>
+bool Vector<T>::remove_first(const T& value) {
+	for (std::size_t i = 0; i < size; ++i) {
+		if (arr[i] == value) {
+			for (std::size_t j = i; j < size - 1; ++j) {
+				arr[j] = arr[j + 1];
+			}
+			size--;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	Vector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.print();
+
+	vec.insert(1, 4);
+	vec.print();
+
+	vec.remove_first(2);
+	if (!vec.has_item(2)) vec.print();
+	std::cout << vec.get_size() << std::endl;
+
+	return 0;
+}
