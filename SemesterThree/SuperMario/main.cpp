fa2dec795cc9146e1f29a7a58b7eb68ad983157c
K3nch <gecayon.k3nch@yandex.ru>
2025-09-12 19:58:47 +0300

    added mario object

--- SemesterThree/SuperMario/main.cpp
+++ SemesterThree/SuperMario/main.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <math.h>
+
 #define mapWidth 80
 #define mapHeight 25
 
+typedef struct SObject {
+    float x, y;
+    float width, height;
+} TObject;
+
 char map[mapHeight][mapWidth + 1];
+TObject mario;
 
 void ClearMap()
 {
@@ -19,9 +27,37 @@ void ShowMap()
     for (int j = 0; j < mapHeight; j++) printf("%s", map[j]);
 }
 
+void SetObjectPos(TObject *obj, float xPos, float yPos)
+{
+    (*obj).x = xPos;
+    (*obj).y = yPos;
+}
+
+void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight)
+{
+    SetObjectPos(obj, xPos, yPos);
+    (*obj).width = oWidth;
+    (*obj).height = oHeight;
+
+}
+
+void PutObjectOnMap(TObject obj)
+{
+    int ix = (int)round(obj.x);
+    int iy = (int)round(obj.y);
+    int iWidth = (int)round(obj.width);
+    int iHeight = (int)round(obj.height);
+
+    for (int i = ix; i < (ix + iWidth); i++)
+        for (int j = iy; j < (iy + iHeight); j++)
+            map[j][i] = '@';
+}
+
 int main()
 {
+    InitObject(&mario, 39, 10, 3, 3);
     ClearMap();
+    PutObjectOnMap(mario);
     ShowMap();
     return 0;
 }
