902b791c066de0b19695b207b7eea3cd8d76d698
K3nch <gecayon.k3nch@yandex.ru>
2025-09-14 21:05:44 +0300

    Rewrite visual via SFML

--- SemesterThree/SuperMario/main.cpp
+++ SemesterThree/SuperMario/main.cpp
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <math.h>
+#include <SFML/Graphics.hpp>
+#include <cmath>
 
 #define mapWidth 80
 #define mapHeight 25
+#define cellSize 20
 
 typedef struct SObject {
     float x, y;
@@ -12,52 +11,66 @@ typedef struct SObject {
 } TObject;
 
 char map[mapHeight][mapWidth + 1];
+sf::RenderWindow window(sf::VideoMode(mapWidth * cellSize, mapHeight * cellSize), "SuperMario SFML");
 TObject mario;
 
 void ClearMap()
 {
-    for (int i = 0; i < mapWidth; i++) map [0][i] = '.';
-    map[0][mapWidth] = '\0';
-    for (int j = 0; j < mapHeight; j++) sprintf(map[j], map[0]);
+    window.clear(sf::Color::Black);
 }
 
 void ShowMap()
 {
-    map[mapHeight - 1][mapWidth - 1] = '\0';
-    for (int j = 0; j < mapHeight; j++) printf("%s", map[j]);
+    window.display();
 }
 
 void SetObjectPos(TObject *obj, float xPos, float yPos)
 {
-    (*obj).x = xPos;
-    (*obj).y = yPos;
+    obj->x = xPos;
+    obj->y = yPos;
 }
 
 void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight)
 {
     SetObjectPos(obj, xPos, yPos);
-    (*obj).width = oWidth;
-    (*obj).height = oHeight;
-
+    obj->width = oWidth;
+    obj->height = oHeight;
 }
 
 void PutObjectOnMap(TObject obj)
 {
-    int ix = (int)round(obj.x);
-    int iy = (int)round(obj.y);
-    int iWidth = (int)round(obj.width);
-    int iHeight = (int)round(obj.height);
+    int ix = (int)round(obj.x) * cellSize;
+    int iy = (int)round(obj.y) * cellSize;
+    int iWidth = (int)round(obj.width) * cellSize;
+    int iHeight = (int)round(obj.height) * cellSize;
+    sf::VertexArray vertices(sf::PrimitiveType::Points);
 
     for (int i = ix; i < (ix + iWidth); i++)
         for (int j = iy; j < (iy + iHeight); j++)
-            map[j][i] = '@';
+            vertices.append({{(float)i, (float)j}, sf::Color::White, {(float)i, (float)j}});
+
+    window.draw(vertices);
 }
+
 
 int main()
 {
+
     InitObject(&mario, 39, 10, 3, 3);
-    ClearMap();
-    PutObjectOnMap(mario);
-    ShowMap();
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if ((event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) || event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        ClearMap();
+        PutObjectOnMap(mario);
+        ShowMap();
+    }
+
     return 0;
 }
